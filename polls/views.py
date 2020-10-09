5c85b0dbd6f6ca92ac4318eb11857cbabb46e9f1
Matt Mont <mattmont415@gmail.com>
2020-10-08 18:21:46 -0700

    Before updating Tutorial 4, Generic Views

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,13 @@
 #Common to get an object or raise 404, django's shortcut
 from django.shortcuts import render, get_object_or_404
 #Http404 needs to be imported to handle 404 errorss
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 #this is for loading up a template :)
 from django.template import loader
+from django.urls import reverse
 
-from .models import Question
+#Import each model that will be used in the views
+from .models import Choice, Question
 
 #A view is a type of Web page in Django app that serves a specific function with template
 #Web pages and other content are delivered by views
@@ -42,11 +44,34 @@ def detail(request, question_id):
   # return render(request, 'polls/detail.html', {'question': question})
   #this can be rewritten as...
   question = get_object_or_404(Question, pk=question_id) #raise 404 if does not exist
-  return render(request, 'polls/detail,html', {'question': question})
+  return render(request, 'polls/detail.html', {'question': question})
 
+#Request is an HttpRequest object.
+#After somebody votes, the vote view redirects to te results page
 def results(request, question_id):
-  response = "You're looking at the results of question %s."
-  return HttpResponse(response % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-  return HttpResponse("You're voting on question %s." % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  try:
+    #request.POST dictionary-like object that lets you access submitted data
+    #...POST['choice'] returns the ID of the selected choice as string (always string)
+    #Choice is from the form name="choice" in detail.html
+    selected_choice = question.choice_set.get(pk=request.POST['choice'])
+  #raises error if Choice was not provided in POST data
+  except (KeyError, Choice.DoesNotExist):
+    return render(request, 'polls/detail.html', {
+      'question': question,
+      'error_message': "You didn't make a CHOICE!",
+    })
+  else:
+    #Increments the choice count
+    selected_choice.votes += 1
+    selected_choice.save()
+    #Always return HttpResponseRedirect after DEALING WITH POST DATA
+    #Data will not be posted twice this way!
+    #This takes a single argument, the URL the user will be redirected to
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    #Reverse is given the name of the view we want to pass control to
+    #Reverse will return a string like '/polls/3/results/'
