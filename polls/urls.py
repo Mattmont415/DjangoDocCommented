bab6a68ce1da39d3ac153eae9a1fdc4305b39d76
Matt Mont <mattmont415@gmail.com>
2020-10-08 17:28:11 -0700

    Working on Tutorial 4, updated to serve so far

--- polls/urls.py
+++ polls/urls.py
@@ -4,11 +4,15 @@ from django.urls import path
 
 from . import views
 
+#use namespacing to prevent duplicates, for instance if another app had a 'detail' name
+app_name = 'polls'
+
 urlpatterns = [
   #/polls/
   path('', views.index, name='index'),
 
-  #/polls/5
+  #/polls/5, the 'name' value will be called by the {% url %} template tag
+  #Easier to modify pathname here, and not hardcoded all over documents
   path('<int:question_id>/', views.detail, name='detail'),
   #/polls/4/results/
   path('<int:question_id>/results/', views.results, name='results'),
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+#Common to get an object or raise 404, django's shortcut
+from django.shortcuts import render, get_object_or_404
+#Http404 needs to be imported to handle 404 errorss
+from django.http import HttpResponse, Http404
 #this is for loading up a template :)
 from django.template import loader
 
@@ -17,17 +19,30 @@ from .models import Question
 #This will display the latest 5 poll questions, separated by commas, according to publication date
 def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
-  template = loader.get_template('polls/index.html')
-  context = { #Loads tempalte and PASSES it a context: a dict mapping template variable
-  #names to python objects
-    'latest_question_list': latest_question_list
-  }
-  return HttpResponse(template.render(context, request))
+  # template = loader.get_template('polls/index.html')
+  # context = { #Loads template and PASSES it a context: a dict mapping template variable
+  # #names to python objects
+  #   'latest_question_list': latest_question_list
+  # }
+  # return HttpResponse(template.render(context, request))
+  #The above lines rewritten as...
+  context = {'latest_question_list': latest_question_list}
+  #The render function takes the REQUEST object, a template name, and dict, it 
+  #returns an HTTPResponse object of the given template rendered WITh that context
+  return render(request, 'polls/index.html', context)
 
 #Called to object from address bar
 #detail(request=<HttpRequest object>, question_id=34)
 def detail(request, question_id):
-  return HttpResponse("This is question %s." % question_id)
+  # try:
+  #   question = Question.objects.get(pk=question_id)
+  # except Question.DoesNotExist:
+  #   #Raises 404 exception if a question with requested ID does not exist
+  #   raise Http404('Question does not exist')
+  # return render(request, 'polls/detail.html', {'question': question})
+  #this can be rewritten as...
+  question = get_object_or_404(Question, pk=question_id) #raise 404 if does not exist
+  return render(request, 'polls/detail,html', {'question': question})
 
 def results(request, question_id):
   response = "You're looking at the results of question %s."
